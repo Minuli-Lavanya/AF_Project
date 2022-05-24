0d4e5aeab99049c89a6ad128a6107a5fa01717f6
Lakma <lakmagamage97@gmail.com>
2022-05-24 19:21:58 +0530

    frontend boiler template

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from "./components/Home";
+
+
+
+const App=()=>{
+  return (
+      <div>
+      <BrowserRouter>
+          <Routes>
+
+                  
+    
+                  <Route path="/" element= {<Home/>}/>
+                  
+
+
+       
+          </Routes>
+      </BrowserRouter>
+      </div>
+  );
+}
+
+
+  
+export default App;
