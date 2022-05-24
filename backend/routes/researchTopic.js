2b670a2ff120688a6154ef45a78cc73908330727
Lakma <lakmagamage97@gmail.com>
2022-05-24 19:24:43 +0530

    update research topic detail route

--- backend/routes/researchTopic.js
+++ backend/routes/researchTopic.js
@@ -1,31 +1,37 @@
 const router = require("express").Router();
-const {response} = require("express");
+const { response } = require("express");
 let ResearchTopic = require("../models/ResearchTopic");
 
 router.route("/add").post((req, res) => {
 
+
+
+    const researchTitle = req.body.researchTitle;
+    const research_category = req.body.research_category;
+    const research_area = req.body.research_area;
+    const supervisor_name = req.body.supervisor_name;
+    const co_supervisor_name = req.body.co_supervisor_name;
     const groupId = req.body.groupId;
     const member1 = req.body.member1;
+    const leader_email = req.body.leader_email;
+    const leader_phoneNumber = req.body.leader_phoneNumber;
     const member2 = req.body.member2;
     const member3 = req.body.member3;
     const member4 = req.body.member4;
-    const email = req.body.email;
-    const phoneNumber = req.body.phoneNumber;
-    const researchTitle = req.body.researchTitle;
-    const desription = req.body.desription;
-    const status = req.body.status;
 
     const newResearchTopic = new ResearchTopic({
+        researchTitle,
+        research_category,
+        research_area,
+        supervisor_name,
+        co_supervisor_name,
         groupId,
         member1,
+        leader_email,
+        leader_phoneNumber, 
         member2,
         member3,
-        member4,
-        email,
-        phoneNumber,
-        researchTitle,
-        desription,
-        status
+        member4 
     })
 
     newResearchTopic.save().then(() => {
@@ -50,19 +56,34 @@ router.route("/update/:id").put(async (req,res) => {
 
     let userId = req.params.id;
 
-    const {groupId,member1,member2,member3,member4,email,phoneNumber,researchTitle,desription,status} = req.body;
-
-    const updateResearchTopic = {
+    const {
+        researchTitle,
+        research_category,
+        research_area,
+        supervisor_name,
+        co_supervisor_name,
         groupId,
         member1,
+        leader_email,
+        leader_phoneNumber, 
         member2,
         member3,
-        member4,
-        email,
-        phoneNumber,
+        member4 
+    } = req.body;
+
+    const updateResearchTopic = {
         researchTitle,
-        desription,
-        status
+        research_category,
+        research_area,
+        supervisor_name,
+        co_supervisor_name,
+        groupId,
+        member1,
+        leader_email,
+        leader_phoneNumber, 
+        member2,
+        member3,
+        member4 
     }
 
     const update = await ResearchTopic.findByIdAndUpdate(userId, updateResearchTopic).then(() => {
@@ -73,6 +94,31 @@ router.route("/update/:id").put(async (req,res) => {
     }) 
 
 })
+
+router.route("/delete/:id").delete(async (req, res) => {
+    let userId = req.params.id;
+
+    await ResearchTopic.findByIdAndDelete(userId).then(() => {
+        res.status(200).send({status: " Deleted "});
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status: "Error with delete !", error:err.message})
+    })
+})
+
+//get one's data
+router.route("/get/:id").get(async (req, res) => {
+    let userId = req.params.id;
+
+    const researchtopicdata = await ResearchTopic.findById(userId).then((researchTopic) => {
+        res.status(200).send({status: "fetched", researchTopic});
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status: "Error with get details!", error:err.message});
+    })
+
+})
+
 
 
 // export the module
