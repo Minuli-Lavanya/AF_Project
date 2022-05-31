d88ff4da54387f86dc8b4acebb23d828a2f8cf1e
Lakma <lakmagamage97@gmail.com>
2022-05-31 23:51:45 +0530

    create routes for supervisor management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "./components/Home";
+import AddReseachTopic from "./components/ResearchTopic/AddResearchTopic";
+import ViewAllResearchTopic from "./components/ResearchTopic/AllResearchTopic";
+import EditResearchTopic from "./components/ResearchTopic/EditResearchTopic";
+import AddSupervisorDetails from "./components/SupervisorDetails/AddSupervisorDetails";
+import AllSupervisor from "./components/SupervisorDetails/AllSupervisorDetails";
+import EditSupervisor from "./components/SupervisorDetails/EditSupervisor";
 
 
 
@@ -14,8 +20,14 @@ const App=()=>{
                   
     
                   <Route path="/" element= {<Home/>}/>
-                  
 
+                  <Route path="/AddReseacrchTopic" element= {<AddReseachTopic/>}/>
+                  <Route path="/ViewAllResearchTopic" element ={<ViewAllResearchTopic/>}/>
+                  <Route path="/EditResearchTopic/:id" element ={<EditResearchTopic/>}/>
+                  
+                  <Route path="/AddSupervisorDetails" element ={<AddSupervisorDetails/>}/>
+                  <Route path="/AllSupervisorDetails" element ={<AllSupervisor/>}/>
+                  <Route path="/EditSupervisorDetails/:id" element ={<EditSupervisor/>}/>
 
        
           </Routes>
